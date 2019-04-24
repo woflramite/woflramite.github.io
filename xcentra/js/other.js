6cb625a06e6e6e286062979dfbf10f01f3be7282
Balya Ibnu Sulistiyono <33630323+woflramite@users.noreply.github.com>
2019-04-24 22:50:34 +0700

    Added POST mailing list feature
    Added mailing list feature to Firebase

--- xcentra/js/other.js
+++ xcentra/js/other.js
@@ -1,20 +1,9 @@
-function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
-
-function submit() {
-  var $result = $("#result");
-  var email = $("#email").val();
-  $result.text("");
-
-  if (validateEmail(email)) {
-    $result.
-  } else {
-    $result.text("false");
-    $result.css("color", "red");
-  }
-  return false;
-}
-
-$("#button").on("click", submit);
+var config = {
+	apiKey: "AIzaSyAwUvM3lPpK9U5LN5GNcNyUo13z4alVkvc",
+	authDomain: "xcentra-mailing-list.firebaseapp.com",
+	databaseURL: "https://xcentra-mailing-list.firebaseio.com",
+	projectId: "xcentra-mailing-list",
+	storageBucket: "xcentra-mailing-list.appspot.com",
+	messagingSenderId: "49675923395"
+};
+firebase.initializeApp(config);
---
+++ xcentra/js/saveToFirebase.js
@@ -0,0 +1,16 @@
+function saveToFirebase(email,name) {
+    var guestObject = {
+        email: email;
+        name: name
+    };
+
+    firebase.database().ref('subscription-entries').push().set(guestObject)
+        .then(function(snapshot) {
+            success(); // some success method
+        }, function(error) {
+            console.log('error' + error);
+            error(); // some error method
+        });
+}
+
+saveToFirebase(email);
